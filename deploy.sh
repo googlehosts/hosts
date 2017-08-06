ccdb5a6162a5cc3a496a5682023e9553afa91774
beyondgfw <beyond.gfw.limit@gmail.com>
2017-08-06 12:07:18 +0800

    Colored debug output.

--- deploy.sh
+++ deploy.sh
@@ -1,52 +1,56 @@
 #!/bin/bash
-set -e
 
-echo Generating hosts files.
+set -e
+function echogr {
+	echo -e \\033[0\;32m$@\\033[0m
+}
+
+echogr Generating hosts files.
 mkdir output
 node generate.js hosts.yml $(git show -s --format=%cd --date=short) output
 
 if [ "$PUSH_TO_MASTER" == "true" ]; then
-	echo Importing SSH key.
+	echogr Importing SSH key.
 	base64 -d <<< $SSH_KEY > ~/.ssh/id_ed25519
 	chmod 600 ~/.ssh/id_ed25519
 
-	echo Importing GPG key.
+	echogr Importing GPG key.
 	base64 -d <<< $GPG_KEY | gpg --import
 
-	echo Cloning master branch.
+	echogr Cloning master branch.
 	git clone git@github.com:$TRAVIS_REPO_SLUG master
 
 	cp output/* master/
 	cd master
 	if [ -n "$(git status --porcelain)" ]; then
-		echo Changes detected.
+		echogr Changes detected.
 
-		echo Configuring git.
+		echogr Configuring git.
 		git config user.name $(git show -s --format="%aN" $TRAVIS_COMMIT)
 		git config user.email $(git show -s --format="%aE" $TRAVIS_COMMIT)
 		git config user.signingkey $GPG_KEY_ID
 		git config push.default simple
 
-		echo Git configured.
+		echogr Git configured.
 		cat .git/config
 
-		echo Commiting changes.
+		echogr Commiting changes.
 		git add -A
 		if [ "$(git log --oneline $TRAVIS_COMMIT_RANGE | wc -l)" == "1" ]; then
-			echo Changes are from a single commit.
+			echogr Changes are from a single commit.
 			git show -s --format="%B" $TRAVIS_COMMIT > commit-msg.tmp
 		else
-			echo Changes are from multiple commits.
+			echogr Changes are from multiple commits.
 			printf "Multiple commits from hosts-source.\n\n" > commit-msg.tmp
 			git log --format="%H %s" $TRAVIS_COMMIT_RANGE >> commit-msg.tmp
 		fi
 		GIT_COMMITTER_DATE=$(git show -s --format="%cD" $TRAVIS_COMMIT) git commit -S -F commit-msg.tmp
 
-		echo Changes committed, pushing.
+		echogr Changes committed, pushing.
 		git push
 	else
-		echo No changes detected, deployment skipped.
+		echogr No changes detected, deployment skipped.
 	fi
 else
-	echo Running in a local environment, deployment skipped.
+	echogr Running in a local environment, deployment skipped.
 fi
