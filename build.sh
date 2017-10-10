656fea52217724ac3a551963d5243263fe4e6e2f
beyondgfw <beyond.gfw.limit@gmail.com>
2017-10-10 21:00:23 +0800

    Make deploy.sh work in more environments, rename it to build.sh
    build.sh should take the place of generate-local.sh.

--- build.sh
+++ build.sh
@@ -4,12 +4,29 @@ set -e
 function echogr {
 	echo -e \\033[32m$@\\033[0m
 }
+function echoye {
+	echo -e \\033[33m$@\\033[0m
+}
 
 echogr Generating hosts files.
-mkdir output
-node src/generate.js "$PWD/data" $(git show -s --format=%cd --date=short) "$PWD/output"
+mkdir -p output
+if command -v git > /dev/null; then
+	DATE=$(git show -s --format=%cd --date=short)
+else
+	echoye Git not found, using current date for the \"Last updated\" field.
+	DATE=$(date +%Y-%m-%d)
+fi
+node src/generate.js "$PWD/data" $DATE "$PWD/output"
 
-if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+if [ "$TRAVIS" != "true" ]; then
+	echoye Not running on Travis CI, deployment skipped.
+elif [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+	echoye Building for a pull request, deployment skipped.
+elif [ "$TRAVIS_BRANCH" != "hosts-source" ]; then
+	echoye Bulding for a branch other than \"hosts-source\", deployment skipped.
+else
+	echogr Starting deployment.
+
 	echogr Importing SSH key.
 	base64 -d <<< $SSH_KEY > ~/.ssh/id_ed25519
 	chmod 600 ~/.ssh/id_ed25519
@@ -52,6 +69,4 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
 	else
 		echogr No changes detected, deployment skipped.
 	fi
-else
-	echogr Running in an untrusted environment, deployment skipped.
 fi
--- generate-local.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-if [ -e output ]; then
-	rm -rf output
-fi
-mkdir output
-node src/generate.js "$PWD/data" $(git show -s --format=%cd --date=short) "$PWD/output"
