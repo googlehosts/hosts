36c39adefd67cb66364be518d5f6a7f52f0708db
ziyuedi <ziyuedi@users.noreply.github.com>
2019-01-26 00:50:15 +0800

    hosts-blank.js
    Add a hosts generator that uses blanks

---
+++ src/generators/hosts-blank.js
@@ -0,0 +1,12 @@
+module.exports = {
+	fileName: 'hosts',
+	generate: (function () {
+		var generateEntry = function (ip, domain) {
+			return `${ip} ${domain}`;
+		};
+		var generateComment = function (data) {
+			return data.replace(/^(.)/gm, '# $1');
+		};
+		return require('../generate-helper')(generateEntry, generateComment);
+	})()
+};
